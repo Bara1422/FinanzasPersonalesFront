0294baa994a1ab6a3709857046612c34caef6dc8
Juan Baranovsky <xxbaraxx@hotmail.com>
2026-04-10 19:56:01 -0300

    fix: merged changes between dev and tokenExpired branches

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   }, [fetchUserData]);
 
   if (!isHydrated) {
-    return <Spinner />;
+    return <Spinner className="size-8 grid place-items-center m-auto h-dvh" />;
   }
   return (
     <BrowserRouter>
